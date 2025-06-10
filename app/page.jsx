908df4b5d2f46d4d8d32a1d4815bff8ec89c7580
Muhammad Jazib <mjxdex@gmail.com>
2025-06-10 17:25:54 +0500

    v2

--- app/page.jsx
+++ app/page.jsx
@@ -9,8 +9,12 @@ const page = () => {
     e.preventDefault();
     const formdata = new FormData(e.target);
     const paper = formdata.get("paper");
-    setPapers(prev => [...prev, paper]);
-    e.target.reset();
+    if (paper.length > 2) {
+      setPapers(prev => [...prev, paper]);
+      e.target.reset();
+    } else {
+      alert("Paper can not be empty.");
+    }
   }
   const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -24,22 +28,26 @@ const page = () => {
       {
         created ?
           <div className="papers">
+            <div className="reset" onClick={() => { [setCreated(false), setSelected(false)] }}>
+              <span>Go back</span>
+            </div>
             {
               papers.map((paper, index) => (
-                <div key={index} className="paper" aria-checked={selected === index} onClick={() => { setSelected(index) }} onDoubleClick={() => { [setCreated(false), setSelected(false)] }}>{paper}</div>
+                <div key={index} className="paper" aria-checked={selected === index} onClick={() => { setSelected(index) }}>{paper}</div>
               ))
             }
           </div> :
           <div className='root'>
             <form onSubmit={createPaper}>
-              <input type="text" placeholder='Enter option' name='paper' />
+              <input type="text" placeholder='Paper value' name='paper' />
             </form>
             {
               papers.map((paper, index) => (
                 <input key={index} type="text" defaultValue={paper} readOnly />
               ))
             }
-            <button onClick={() => { [setCreated(true), setPapers(shuffleArray(papers))] }}>Create Shuffle</button>
+            {papers.length > 1 ? <button onClick={() => { [setCreated(true), setPapers(shuffleArray(papers))] }}>Create Shuffle</button> : <></>}
+            <p className='about'>Many Paper is a helpful tool for situations where someone has multiple choices but isn’t sure which one to pick. Instead of overthinking, the user can simply enter all the possible options as "papers" and let the system shuffle them. This randomization makes it easier to make a fair, unbiased decision — whether it's choosing between tasks, names, ideas, or any kind of options.</p>
           </div>
       }
     </>
